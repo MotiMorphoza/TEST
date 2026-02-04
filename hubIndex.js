9562556456aee41c47f0c99227855af69ff2b2d9
MotiMorphoza <mottex@gmail.com>
2026-02-05 01:31:26 +0200

    Add files via upload

---
+++ hubIndex.js
@@ -0,0 +1,64 @@
+window.HUB_INDEX = {
+  "version": 2,
+  "languages": [
+    {
+      "id": "ar-he",
+      "title": "ar-he"
+    },
+    {
+      "id": "es-he",
+      "title": "es-he"
+    },
+    {
+      "id": "he-en",
+      "title": "he-en"
+    },
+    {
+      "id": "pl-en",
+      "title": "pl-en"
+    }
+  ],
+  "branches": [
+    {
+      "id": "misc",
+      "title": "misc"
+    },
+    {
+      "id": "vocabulary",
+      "title": "vocabulary"
+    }
+  ],
+  "entries": [
+    {
+      "branch": "misc",
+      "group": "important_words",
+      "files": {
+        "ar-he": [
+          "full_house.csv",
+          "political.csv"
+        ],
+        "he-en": [
+          "more-less-too-very-most.csv"
+        ]
+      }
+    },
+    {
+      "branch": "vocabulary",
+      "group": "sentences",
+      "files": {
+        "ar-he": [
+          "000.csv"
+        ],
+        "es-he": [
+          "000.csv"
+        ],
+        "he-en": [
+          "sentences 000.csv"
+        ],
+        "pl-en": [
+          "sentences 000.csv"
+        ]
+      }
+    }
+  ]
+};
